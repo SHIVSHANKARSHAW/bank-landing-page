2f76622a808f1e3f85ebfa031bbea78fb0b11082
Shiv <shivshankarshaw2003@gmail.com>
2025-01-02 09:21:40 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,35 @@
+# Project Hoobank
+
+This is the repository for the Bank Modern Website project.
+
+## Table of Contents
+- [Introduction](#introduction)
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+
+
+## Introduction
+Welcome to the Bank Modern Website project. This project aims to create a modern and responsive banking website.
+
+## Features
+- User-friendly interface
+- Responsive design
+
+## Installation
+To install the project, clone the repository and install the dependencies:
+```bash
+git clone https://github.com/SHIVSHANKARSHAW/project_hoobank-main.git
+cd project_hoobank-main
+npm install
+```
+
+## Usage
+To start the development server, run:
+```bash
+npm run dev
+```
+
+## Contributing
+Contributions are welcome! Please fork the repository and create a pull request.
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react";
+
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
+
+const Navbar = () => {
+  const [active, setActive] = useState("Home");
+  const [toggle, setToggle] = useState(false);
+  const [showNav, setShowNav] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [atTop, setAtTop] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > lastScrollY) {
+        setShowNav(false);
+      } else {
+        setShowNav(true);
+      }
+      setLastScrollY(window.scrollY);
+      setAtTop(window.scrollY === 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [lastScrollY]);
+
+  return (
+    <nav className={` md:pr-16 w-full fixed flex items-center py-2 justify-between navbar z-[100] transition-all duration-500 ease-in-out ${
+          atTop ? 'w-full md:-ml-16 pr-12 md:pr-0' : 'px-2 md:pl-2 w-[90%] md:w-[91%] mt-3 rounded-lg shadow-lg bg-black border-b-2 border-gray-200'
+        } ${showNav ? 'top-0' : '-top-32'}`}>
+      <img src={logo} alt="logo" className="h-20 w-24 md:h-[5rem] md:w-[16rem]" />
+
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] hover:scale-110 ${
+              active === nav.title ? "text-white" : "text-dimWhite"
+            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+            onClick={() => setActive(nav.title)}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle(!toggle)}
+        />
+
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex justify-end items-start flex-1 flex-col">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                  active === nav.title ? "text-white" : "text-dimWhite"
+                } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                onClick={() => setActive(nav.title)}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
